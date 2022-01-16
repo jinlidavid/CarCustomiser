c0cbe3b83d805bf00898a3cb97de5a2e454ad2d1
davidjinli <61616862+davidjinli@users.noreply.github.com>
2022-01-16 22:12:38 +0000

    Completed displayStats method for  Car Struct

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,26 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by David Jin Li on 16/01/2022.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+    
+    func displayStats() -> String{
+        return """
+            Make: \(make)
+            Model: \(model)
+            Top Speed: \(topSpeed)mph
+            Acceleration (0-60): \(acceleration)s
+            Handling: \(handling)
+            """
+    }
+}
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(car.displayStats())
             .padding()
+        
+        
     }
 }
 
