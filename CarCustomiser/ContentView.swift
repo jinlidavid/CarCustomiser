6517ba6d3bc9910aa47474f41f4108eab982de08
davidjinli <61616862+davidjinli@users.noreply.github.com>
2022-01-20 08:37:47 +0000

    Added 4 starter cars and two other toggle switches for updates

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,19 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
-    let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
-
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar: Int = 0 {
+        didSet{
+            if selectedCar >= starterCars.cars.count {
+                selectedCar = 0
+            }
+        }
+    }
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    @State private var drivetrainPackage = false
+    @State private var fuelPackage = false
     
     var body: some View {
-        Text(car.displayStats())
-            .padding()
+        let exhaustPackageBinding = Binding<Bool> (
+            get: {self.exhaustPackage},
+            set: {newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+
+                }
+            }
+        )
         
+        let tiresPackageBinding = Binding<Bool> (
+            get: {self.tiresPackage},
+            set: {newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+
+                }
+            }
+        )
         
+        let drivetrainPackageBinding = Binding<Bool> (
+            get: {self.drivetrainPackage},
+            set: {newValue in
+                self.drivetrainPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration += 0.5
+                } else {
+                    starterCars.cars[selectedCar].acceleration -= 0.5
+
+                }
+            }
+        )
+        
+        let fuelPackageBinding = Binding<Bool> (
+            get: {self.fuelPackage},
+            set: {newValue in
+                self.fuelPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 2
+                    starterCars.cars[selectedCar].acceleration += 0.1
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 2
+                    starterCars.cars[selectedCar].acceleration -= 0.1
+
+
+                }
+            }
+        )
+        
+       
+        Form {
+            VStack (alignment: .leading, spacing: 20){
+                Text("\(starterCars.cars[selectedCar].displayStats())")
+                Button("Next Car", action: {
+                    selectedCar += 1
+                })
+            }
+            Section {
+                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                Toggle("Tires Package", isOn: tiresPackageBinding)
+                Toggle("Drivetrain Package", isOn: drivetrainPackageBinding)
+                Toggle("Fuel Package", isOn: fuelPackageBinding)
+
+
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,15 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by David Jin Li on 19/01/2022.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
+                Car(make: "Volkswagen", model: "Golf", topSpeed: 110, acceleration: 6.2, handling: 7),
+                Car(make: "Toyota", model: "Prius", topSpeed: 132, acceleration: 7.1, handling: 4),
+                Car(make: "Ford", model: "Focus", topSpeed: 120, acceleration: 7.9, handling: 6)]
+}
